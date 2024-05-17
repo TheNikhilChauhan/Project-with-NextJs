af9e5d90fda27b2fee53e9f031a18a6d6e6deee3
Nikhil <ace.nikchauhan@gmail.com>
2024-05-17 13:51:23 +0530

    solving navbar challenge

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <div className="text-2xl text-center">Music Next App</div>
+    </main>
+  );
+};
+export default Home;
