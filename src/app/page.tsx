2b3ef6433eef89b2c0a3ec31fbefd31a91ea33b6
Nikhil <ace.nikchauhan@gmail.com>
2024-05-17 16:50:13 +0530

    solving hero section: spotlight and moving border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import React from "react";
 
 const Home = () => {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <div className="text-2xl text-center">Music Next App</div>
+      <HeroSection />
     </main>
   );
 };
