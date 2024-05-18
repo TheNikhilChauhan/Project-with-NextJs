70b85f45afbc06ff66b5f9135523dd82302c0721
Nikhil <ace.nikchauhan@gmail.com>
2024-05-18 10:28:23 +0530

    added Sticky Scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import StickyScrollSection from "@/components/StickyScrollSection";
 import React from "react";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <StickyScrollSection />
     </main>
   );
 };
